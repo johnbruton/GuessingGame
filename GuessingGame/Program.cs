c9319186f65fe6f72573a4128edc3e5d6bebb9f8
JohnBruton <jbrutoncreative@gmail.com>
2019-01-16 13:15:37 -0500

    first interation complete

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -6,8 +6,21 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Adios!");
+            Console.WriteLine("Please guess a number between 1 and 10.");
+            string userGuess = Console.ReadLine();
+            Console.WriteLine("You guessed: " + userGuess);
+
+            // if the user guess 7
+            if (userGuess.Equals("7"))
+            {
+                // then they win
+                Console.WriteLine("YOU WIN!");
+            }
+            else
+            {
+                // otherwise the lose
+                Console.WriteLine("You lose.");
+            }
         }
     }
 }
